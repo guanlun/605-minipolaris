4b54ccae39b9c4cecc74ffc93200da23d0a88873
Guanlun Zhao <guanlunzhao@gmail.com>
2017-01-27 22:07:55 -0600

    Basic operations

--- gotos/gotos.cc
+++ gotos/gotos.cc
@@ -7,5 +7,14 @@
 #include "Collection/List.h"
 #include  "Statement/Statement.h"
 
+using std::cout;
+using std::endl;
+
 void convert_gotos(ProgramUnit & pgm) {
+    const StmtList& statements = pgm.stmts();
+
+    for (Iterator<Statement> iter = statements; iter.valid(); ++iter) {
+        Statement& stmt = iter.current();
+        cout << iter.current().line() << endl;
+    }
 };
