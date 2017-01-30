bbfc18aa71711e0fb21ea2301331044284116c88
Guanlun Zhao <guanlunzhao@gmail.com>
2017-01-30 00:24:11 -0600

    Insert if statements after arithmetic if block.

--- gotos/gotos.cc
+++ gotos/gotos.cc
@@ -6,15 +6,45 @@
 #include "StmtList.h"
 #include "Collection/List.h"
 #include  "Statement/Statement.h"
+#include "Statement/ArithmeticIfStmt.h"
+#include "Statement/LabelStmt.h"
+#include "Statement/IfStmt.h"
+#include "Statement/EndIfStmt.h"
+#include "Statement/GotoStmt.h"
 
 using std::cout;
 using std::endl;
 
 void convert_gotos(ProgramUnit & pgm) {
-    const StmtList& statements = pgm.stmts();
+    StmtList& statements = pgm.stmts();
 
-    for (Iterator<Statement> iter = statements; iter.valid(); ++iter) {
-        Statement& stmt = iter.current();
-        cout << iter.current().line() << endl;
+    for (Iterator<Statement> iter = statements.stmts_of_type(ARITHMETIC_IF_STMT);
+        iter.valid();
+        ++iter) {
+        ArithmeticIfStmt& aIfStmt = dynamic_cast<ArithmeticIfStmt&>(iter.current());
+
+        Expression& predicate = aIfStmt.expr();
+
+        RefList<Statement> labelList = aIfStmt.label_list();
+
+        LabelStmt& gtStmt = dynamic_cast<LabelStmt&>(labelList[0]);
+        LabelStmt& ltStmt = dynamic_cast<LabelStmt&>(labelList[1]);
+        LabelStmt& eqStmt = dynamic_cast<LabelStmt&>(labelList[2]);
+
+        gtStmt.print(cout);
+        ltStmt.print(cout);
+        eqStmt.print(cout);
+
+        Statement* gotoStmt = new GotoStmt(statements.new_tag(), &gtStmt);
+
+        Statement& newIfStmt = statements.ins_IF_after(predicate.clone(), &aIfStmt);
+
+        statements.ins_after(gotoStmt, &newIfStmt);
+
+
+        /*
+        const Statement* nextStmt = statements.next_ref(aIfStmt);
+        cout << nextStmt->stmt_class() << endl;
+        */
     }
 };
