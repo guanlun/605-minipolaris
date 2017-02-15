cc80808190c6be893db0a5a0e5f1e3bea66e894e
Guanlun Zhao <guanlunzhao@gmail.com>
2017-02-15 10:23:42 -0600

    Implementing CP.

--- constant/ConstPropWS.h
+++ constant/ConstPropWS.h
@@ -3,9 +3,25 @@
 
 class ConstPropWS : public WorkSpace {
 public:
-	ConstPropWS();
-private:
+	ConstPropWS(int pass_tag);
 
+	void add_gen_var(const Symbol& var);
+
+	Listable *listable_clone() const { return NULL; };
+
+	INLINE void print(ostream &o) const { o << "[ ConstPropWS: print not properly implemented. ]"; }
+
+	INLINE int structures_OK() const { return 1; };
+private:
+	RefSet<Symbol> killSet;
+	RefSet<Symbol> genSet;
 };
+
+ConstPropWS::ConstPropWS(int pass_tag) : WorkSpace(pass_tag) {
+}
+
+void ConstPropWS::add_gen_var(const Symbol& var) {
+	genSet.ins(var);
+}
 
 #endif
--- constant/constant.cc
+++ constant/constant.cc
@@ -5,16 +5,29 @@
 #include "constant.h"
 #include "StmtList.h"
 #include "Statement/Statement.h"
+#include "Statement/AssignmentStmt.h"
 #include "Expression/IntConstExpr.h"
+#include "Expression/expr_funcs.h"
 #include "Symbol/SymbolicConstantSymbol.h"
+#include "DictionaryIter.h"
+
+const int PASS_TAG = 2;
 
 void propagate_constants(ProgramUnit & pgm) {
 	StmtList& stmts = pgm.stmts();
 
+	for (DictionaryIter<Symbol> symIter = pgm.symtab().iterator(); symIter.valid(); ++symIter) {
+		Symbol& symbol = symIter.current();
+
+		if (symbol.sym_class() == VARIABLE_CLASS) {
+
+		}
+	}
+
 	for (Iterator<Statement> iter = stmts; iter.valid(); ++iter) {
 		Statement& stmt = iter.current();
 
-		cout << stmt << endl;
+		// cout << stmt << endl;
 
 		for (Iterator<Expression> exprIter = stmt.iterate_expressions();
 			exprIter.valid();
@@ -25,24 +38,37 @@ void propagate_constants(ProgramUnit & pgm) {
 				const Symbol& symbol = expr.symbol();
 
 				if (symbol.sym_class() == SYMBOLIC_CONSTANT_CLASS) {
-					const Expression& constExpr = *(symbol.expr_ref());
-
-					// TODO: change rhs() to something else
-					stmt.rhs(constExpr.clone());
+					const Expression* constExpr = symbol.expr_ref();
 				}
 			}
 		}
 
-//		for (Iterator<Expression> inRefIter = stmt.in_refs();
-//			inRefIter.valid();
-//			++inRefIter) {
-//			Expression& inRefExpr = inRefIter.current();
-//
-//			// inRefExpr.print(cout);
-//			cout << inRefExpr << endl;
-//		}
 
-
-		cout << "----------------------------------------------" << endl;
 	}
+
+	for (Iterator<Statement> iter = stmts; iter.valid(); ++iter) {
+		Statement& stmt = iter.current();
+		stmt.work_stack().push(new ConstPropWS(PASS_TAG));
+	}
+
+	for (Iterator<Statement> iter = stmts; iter.valid(); ++iter) {
+		Statement& stmt = iter.current();
+
+		if (stmt.stmt_class() == ASSIGNMENT_STMT) {
+			RefSet<Statement> predecessors = stmt.pred();
+			cout << stmt.lhs().symbol() << endl;
+
+			for (Iterator<Statement> predStmtIter = predecessors; predStmtIter.valid(); ++predStmtIter) {
+				Statement& predStmt = predStmtIter.current();
+
+				ConstPropWS* predConstPropWS = (ConstPropWS* )predStmt.work_stack().top_ref(PASS_TAG);
+
+				cout << predConstPropWS << endl;
+			}
+
+			cout << "----------------------------------------------" << endl;
+		}
+	}
+
+	// Ref: compute const value: int_const_val in https://parasol.tamu.edu/courses/minipolaris/docs/polaris/_prop_const_w_s_8cc-source.html
 };
