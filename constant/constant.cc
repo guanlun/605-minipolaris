a205b05b4dce0ffa18339cd95302a5c4dd9a0643
Guanlun Zhao <guanlunzhao@gmail.com>
2017-02-15 23:32:45 -0600

    Finally figured out how to substitute consts.

--- constant/constant.cc
+++ constant/constant.cc
@@ -41,16 +41,28 @@ void propagate_constants(ProgramUnit & pgm) {
 	for (Iterator<Statement> iter = stmts; iter.valid(); ++iter) {
 		Statement& stmt = iter.current();
 
-		for (Iterator<Expression> exprIter = stmt.iterate_expressions();
+		for (Mutator<Expression> exprIter = stmt.iterate_expressions();
 			exprIter.valid();
 			++exprIter) {
 			Expression& expr = exprIter.current();
+
+			cout << expr << "     " << expr.arg_list() << endl;
 
 			if (expr.op() == ID_OP) {
 				const Symbol& symbol = expr.symbol();
 
 				if (symbol.sym_class() == SYMBOLIC_CONSTANT_CLASS) {
-					const Expression* constExpr = symbol.expr_ref();
+					cout << "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb" << endl;
+					cout << *(symbol.expr_ref()) << endl;
+
+					exprIter.assign() = symbol.expr_ref()->clone();
+
+//					exprIter.assign() = symbol.expr_ref();
+
+					// const Expression* constExpr = symbol.expr_ref();
+
+//					cout << stmt.rhs() << "                   " << symbol << endl;
+//					substitute_var(&stmt.rhs(), symbol, *constant(1));
 				}
 			}
 		}
@@ -85,7 +97,6 @@ void propagate_constants(ProgramUnit & pgm) {
 			Statement& predStmt = predStmtIter.current();
 
 			ConstPropWS* predConstPropWS = (ConstPropWS*)predStmt.work_stack().top_ref(PASS_TAG);
-
 			stmt_set_intersection(inSet, predConstPropWS->outSet);
 		}
 
@@ -117,6 +128,14 @@ void propagate_constants(ProgramUnit & pgm) {
 		constPropWS->outSet = resultSet;
 
 		cout << "---------------------- end of one stmt ------------------------" << endl;
+	}
+
+	for (Iterator<Statement> iter = stmts; iter.valid(); ++iter) {
+		Statement& stmt = iter.current();
+
+		ConstPropWS* constPropWS = (ConstPropWS*)stmt.work_stack().top_ref(PASS_TAG);
+
+
 	}
 
 	// Ref: compute const value: int_const_val in https://parasol.tamu.edu/courses/minipolaris/docs/polaris/_prop_const_w_s_8cc-source.html
